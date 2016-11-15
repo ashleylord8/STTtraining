eadedb4d3e2433b0bb6c17ddc4b737c5ad3a78e6
Steven Oh <stevenoh93@gmail.com>
2016-11-15 11:16:23 -0500

    Get examples of intents and entities and write to file

--- getExamples.js
+++ getExamples.js
@@ -1,27 +1,50 @@
+// script to create examples.txt for training from the conversation workspace.
+
 var fs = require('fs');
-
-fs.readFile('workspace.json', function (err, data) {
-    if (err) {
-        return console.error(err);
-    }
-    workspace =  JSON.parse(data.toString());
-    //console.log(workspace.intents);
-    var intents = {};
-    var examples = {};
-
-    workspace.intents.forEach( function(item){
-        //console.log (item.intent);
-        //intents += item.intent + '\n';
-
-        item.examples.forEach(function(item) {
-            //console.log (item.text);
-            examples += item.text + '\n';
-        });
-
-    });
-
-    fs.writeFile('examples.txt', examples, function(err) {
-        if (err) throw err;
-        console.log('file saved');
-    });
+var path_intents = './training_data/intent_examples.txt';
+var path_entities = './training_data/entity_examples.txt';
+fs.readFile('./car_workspace.json', function (err, data) {
+  if (err) {
+      return console.error(err);
+  }
+  workspace =  JSON.parse(data.toString());
+  var intents = {};
+  var examples = "";
+  var fd = fs.openSync(path_intents, 'w');
+  writeIntentsToFile(fd);
+  fd = fs.openSync(path_entities, 'w');
+  writeEntitiesToFile(fd);
 });
+
+var writeIntentsToFile = function(fd) {
+  var sep = "";
+  fs.writeSync(fd, '[');
+  workspace.intents.forEach(function(item) {
+    fs.writeSync(fd, sep + JSON.stringify(item, null,' '));
+    if (!sep)
+      sep = ', \n';
+  });
+  fs.writeSync(fd, ']');
+  fs.closeSync(fd);
+}
+
+var writeEntitiesToFile = function(fd) {
+  var sep = "";
+  fs.writeSync(fd, '[');
+  workspace.entities.forEach(function(item) {
+    fs.writeSync(fd, sep + JSON.stringify(item, null,' '));
+    if (!sep)
+      sep = ', \n';
+  });
+  fs.writeSync(fd, ']');
+  fs.closeSync(fd);
+}
+
+// To read examples
+// fs.readFile(path_intents, function (err, data) {
+//   console.log(JSON.parse(data));
+// });
+//
+// fs.readFile(path_entities, function (err, data) {
+//   console.log(JSON.parse(data));
+// });
