6928ca6c75555b7cae0a732f4765765ee12fa270
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-12-30 19:58:31 -0300

    Add missing methods

--- test/test.commands.js
+++ test/test.commands.js
@@ -4,6 +4,7 @@ require('dotenv').config({
   path: path.join(__dirname, '../.env')
 });
 
+const pkg = require('../package.json');
 const Commands = require('../lib/commands');
 
 if (process.env.SPEECH_TO_TEXT_USERNAME) {
@@ -12,32 +13,54 @@ if (process.env.SPEECH_TO_TEXT_USERNAME) {
     password: process.env.SPEECH_TO_TEXT_PASSWORD,
   });
 
-  describe('commands()', () => {
-    it('should list the base models', () => cmd.listModels());
+  describe('commands()', function() {
+    this.slow(1000);
 
-    it('should validate credentials when using the authenticate method', () =>
+    it('listModels()', () => cmd.listModels());
+
+    it('authenticate()', () =>
       cmd.authenticate()
     );
 
-    it('should list the customizations', () => cmd.listCustomizations());
+    it('listCustomizations()', () => cmd.listCustomizations());
 
-    it('should get a customization given a customization id', () =>
+    it('getCustomization()', () =>
       cmd.getCustomization({
         customization_id: process.env.CUSTOMIZATION_ID,
       })
     );
 
-    it('should list customization words given a customization id', () =>
+    it('listCustomizationWords()', () =>
       cmd.listCustomizationWords({
         customization_id: process.env.CUSTOMIZATION_ID,
       })
     );
 
-    it('should list corpora given a customization id', () =>
+    it('listCorpora()', () =>
       cmd.listCorpora({
         customization_id: process.env.CUSTOMIZATION_ID,
       })
     );
+
+    it('createCustomization()', () => {
+      this.timeout(50000);
+      return cmd.createCustomization({
+        workspace: path.join(__dirname, './resources/workspace.json'),
+        customization_name: `${pkg.name}-it`,
+        customization_description: 'it test',
+        base_model_name: 'en-US_NarrowbandModel',
+      }).then(newCustomization => cmd.deleteCustomization(newCustomization));
+    });
+
+    // it('createAndTrainCustomization()', () => {
+    //   this.timeout(50000);
+    //   return cmd.createAndTrainCustomization({
+    //     workspace: path.join(__dirname, './resources/workspace.json'),
+    //     customization_name: `${pkg.name}-it`,
+    //     customization_description: 'it test',
+    //     base_model_name: 'en-US_NarrowbandModel',
+    //   }).then(newCustomization => cmd.deleteCustomization(newCustomization));
+    // });
   });
 }
 else {
